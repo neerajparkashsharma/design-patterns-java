7bafdde3ceb1362e8df90c4725b74ed1afff38c5
Neeraj Parkash Sharma <npsstudies@gmail.com>
2023-03-06 23:56:50 +0500

    strategy pattern update

---
+++ src/main/java/org/example/strategypattern/StrategyPattern.java
@@ -0,0 +1,15 @@
+package org.example.strategypattern;
+
+public class StrategyPattern {
+    public static void main(String[] args) {
+        Context context = new Context(new OperationAddition());
+        System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationSubtraction());
+        System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiplication());
+        System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+    }
+    
+}
