e60b8c1f5f6edcccc5cf785b47c3b2d1dd5b2528
Neeraj Parkash <neeraj.parkash@paysyslabs.comh>
2023-03-28 11:46:17 +0500

    added a context in StrategyPattern

--- src/main/java/org/example/strategypattern/StrategyPattern.java
+++ src/main/java/org/example/strategypattern/StrategyPattern.java
@@ -2,6 +2,7 @@ package org.example.strategypattern;
 
 public class StrategyPattern {
     public static void main(String[] args) {
+
         Context context = new Context(new OperationAddition());
         System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
 
@@ -10,6 +11,9 @@ public class StrategyPattern {
 
         context = new Context(new OperationMultiplication());
         System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+
+        context = new Context(new OperationMultiplication());
+        System.out.println("10 * 2 = " + context.executeStrategy(10, 2));
     }
 
 }
